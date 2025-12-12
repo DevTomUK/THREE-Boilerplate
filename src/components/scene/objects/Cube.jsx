6956940c7a7f5a210d8f8c7b550afe462d235e9a
tomghdev <tomghdev@gmail.com>
2025-12-12 07:45:28 +0000

    updates cube with onClick to demo camera movements

--- src/components/scene/objects/Cube.jsx
+++ src/components/scene/objects/Cube.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CameraContext } from '../../../context/cameraContext'
 
 export default function Cube() {
 
   const { cameraRef, setTargetPosition } = useContext(CameraContext)
 
+  const [position, setPosition] = useState(0)
+
   function handleClickCube() {
-    setTargetPosition([5, 5, 5])
+    switch(position) {
+      case 0:
+        setTargetPosition([5, 3, 5])
+        break;
+      case 1:
+        setTargetPosition([2, 5, 8])
+        break;
+      case 2:
+        setTargetPosition([6, 5, 6])
+        break;
+    }
+    setPosition(curr => (curr + 1) % 3)
   }
 
   return (
