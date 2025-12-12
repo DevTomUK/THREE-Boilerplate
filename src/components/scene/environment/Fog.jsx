4d0ee01566b7544cef1580e5054db52544549116
DevTomUK <tomghdev@gmail.com>
2025-12-12 13:21:40 +0000

    changes fog to allow modes - exponential, linear, etc

--- src/components/scene/environment/Fog.jsx
+++ src/components/scene/environment/Fog.jsx
@@ -1,10 +1,9 @@
-export default function Fog({
-  color = 'grey',
-  near = 5,
-  far = 20,
-  enabled = true,
-}) {
+export default function Fog({ mode = 'linear', color = 'grey', near = 5, far = 20, density = 0.05, enabled = true }) {
   if (!enabled) return null;
+
+  if (mode === 'exp') {
+    return <fogExp2 attach="fog" color={color} density={density} />;
+  }
 
   return <fog attach="fog" color={color} near={near} far={far} />;
 }
