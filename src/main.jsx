bf5c71d73b20a0236fff3e868c2327693f203384
DevTomUK <tomghdev@gmail.com>
2025-12-14 15:07:32 +0000

    adds a game component which renders instead of demo if uncommented

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import T3Scene from './T3Scene.jsx';
+import Game from './components/Game.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <T3Scene />
+  <T3Scene>
+    {/* <Game /> */}
+  </T3Scene>
 );
