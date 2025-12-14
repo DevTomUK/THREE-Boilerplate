9400065aad0d96ef9859dde01aa05974fb4501d1
DevTomUK <tomghdev@gmail.com>
2025-12-14 10:14:59 +0000

    updates camera component to allow choice of drei camera types

--- src/components/core/Camera.jsx
+++ src/components/core/Camera.jsx
@@ -1,14 +1,44 @@
-import { PerspectiveCamera } from "@react-three/drei";
+// Provides a camera from drei
+// Type can be set to: "perspective" | "orthographic" | "cube"
+// Props can be passed in from parent to customise
+
+import {
+  PerspectiveCamera,
+  OrthographicCamera,
+  CubeCamera,
+} from "@react-three/drei";
 import { useContext } from "react";
 import { CameraContext } from "../../context/cameraContext";
 
-export default function Camera() {
+export default function Camera({ type = "perspective", ...props }) {
   const { cameraRef } = useContext(CameraContext);
 
-  return (
-    <PerspectiveCamera
-      makeDefault
-      ref={cameraRef}
-    />
-  );
+  switch (type) {
+    case "orthographic":
+      return (
+        <OrthographicCamera
+          makeDefault
+          ref={cameraRef}
+          {...props}
+        />
+      );
+
+    case "cube":
+      return (
+        <CubeCamera
+          ref={cameraRef}
+          {...props}
+        />
+      );
+
+    case "perspective":
+    default:
+      return (
+        <PerspectiveCamera
+          makeDefault
+          ref={cameraRef}
+          {...props}
+        />
+      );
+  }
 }
