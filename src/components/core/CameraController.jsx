93d2586fabfa7cc7b52eaffddfae34e889d79fc0
DevTomUK <tomghdev@gmail.com>
2025-12-14 10:18:07 +0000

    adds info to CameraController

--- src/components/core/CameraController.jsx
+++ src/components/core/CameraController.jsx
@@ -1,13 +1,18 @@
+// Uses the scene's camera reference to update position and target
+// useFrame is used to smoothly transition with LERP
+
 import { useFrame } from "@react-three/fiber";
 import { useContext, useRef } from "react";
 import { CameraContext } from "../../context/cameraContext";
 import * as THREE from "three";
 
 export default function CameraController() {
-  const { cameraRef, targetPosition, targetLookAt } = useContext(CameraContext);
+  const { cameraRef, targetPosition, targetLookAt } =
+    useContext(CameraContext);
 
-  const currentLookAt = useRef(new THREE.Vector3(...targetLookAt));
-  const targetPos = useRef(new THREE.Vector3(...targetPosition));
+  const currentLookAt = useRef(new THREE.Vector3());
+  const targetLookAtVec = useRef(new THREE.Vector3());
+  const targetPos = useRef(new THREE.Vector3());
 
   useFrame(() => {
     if (!cameraRef.current) return;
@@ -15,7 +20,8 @@ export default function CameraController() {
     targetPos.current.set(...targetPosition);
     cameraRef.current.position.lerp(targetPos.current, 0.05);
 
-    currentLookAt.current.lerp(new THREE.Vector3(...targetLookAt), 0.05);
+    targetLookAtVec.current.set(...targetLookAt);
+    currentLookAt.current.lerp(targetLookAtVec.current, 0.05);
     cameraRef.current.lookAt(currentLookAt.current);
   });
 
