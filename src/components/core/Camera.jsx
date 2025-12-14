619d64bcc191d840cf75d64460914c8c26d54cee
DevTomUK <tomghdev@gmail.com>
2025-12-14 13:35:16 +0000

    makes Camera npm ready

--- src/components/core/Camera.jsx
+++ src/components/core/Camera.jsx
@@ -1,44 +1,34 @@
 // Provides a camera from drei
-// Type can be set to: "perspective" | "orthographic" | "cube"
-// Props can be passed in from parent to customise
+// Supports "perspective" | "orthographic" | "cube"
+// Fully configurable via options prop
 
-import {
-  PerspectiveCamera,
-  OrthographicCamera,
-  CubeCamera,
-} from "@react-three/drei";
 import { useContext } from "react";
+import { PerspectiveCamera, OrthographicCamera, CubeCamera } from "@react-three/drei";
 import { CameraContext } from "../../context/cameraContext";
+import merge from "lodash.merge";
 
-export default function Camera({ type = "perspective", ...props }) {
+export default function Camera({ type = "perspective", options = {} }) {
   const { cameraRef } = useContext(CameraContext);
+
+  // Default options per camera type
+  const defaultOptions = {
+    perspective: { fov: 50, near: 0.1, far: 1000, position: [0, 0, 10] },
+    orthographic: { makeDefault: true, near: 0.1, far: 1000, position: [0, 0, 10], zoom: 1 },
+    cube: { position: [0, 0, 10] },
+  };
+
+  // Merge defaults with user options
+  const config = merge({}, defaultOptions[type], options);
 
   switch (type) {
     case "orthographic":
-      return (
-        <OrthographicCamera
-          makeDefault
-          ref={cameraRef}
-          {...props}
-        />
-      );
+      return <OrthographicCamera ref={cameraRef} {...config} makeDefault />;
 
     case "cube":
-      return (
-        <CubeCamera
-          ref={cameraRef}
-          {...props}
-        />
-      );
+      return <CubeCamera ref={cameraRef} {...config} />;
 
     case "perspective":
     default:
-      return (
-        <PerspectiveCamera
-          makeDefault
-          ref={cameraRef}
-          {...props}
-        />
-      );
+      return <PerspectiveCamera ref={cameraRef} {...config} makeDefault />;
   }
 }
