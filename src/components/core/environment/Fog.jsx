413cbd9e652ec6ebbe6aaafff1b822451e11b8f4
DevTomUK <tomghdev@gmail.com>
2025-12-14 10:30:14 +0000

    updates Fog with description

--- src/components/core/environment/Fog.jsx
+++ src/components/core/environment/Fog.jsx
@@ -1,16 +1,20 @@
+// Provides scene fog - linear and exponential fog modes can be passed in with mode
+
 export default function Fog({
   enabled = true,
-  mode = 'linear',   
-  color = '#dddddd',
-  near = 1,           
-  far = 1000,          
-  density = 0.0005,     
+  mode = "linear",
+  color = "#dddddd",
+  near = 1,
+  far = 1000,
+  density = 0.0005,
 }) {
   if (!enabled) return null;
 
-  return mode === 'exp' ? (
-    <fogExp2 attach="fog" color={color} density={density} />
-  ) : (
-    <fog attach="fog" color={color} near={near} far={far} />
-  );
+  // Exponential fog (FogExp2)
+  if (mode === "exp") {
+    return <fogExp2 attach="fog" color={color} density={density} />;
+  }
+
+  // Linear fog (Fog)
+  return <fog attach="fog" color={color} near={near} far={far} />;
 }
